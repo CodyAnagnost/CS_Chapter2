1166c9f5a7eb2e0b556191102e97a4e0b87bdc07
CodyAnagnost <codyanagnost@hotmail.com>
2017-02-03 17:20:08 -0500

    eggggssssszzzz

--- CS_Chapter2/Eggs/Program.cs
+++ CS_Chapter2/Eggs/Program.cs
@@ -9,6 +9,12 @@ namespace Eggs
     {
         static void Main(string[] args)
         {
+            int Dinkleburg = 8;
+            int Drake =  4;
+            int Sean = 9;
+            int Cody = 2;
+            int eggs = Dinkleburg + Drake + Sean + Cody;
+            Console.WriteLine("Total number of eggs is " + eggs + " Dozens " + (eggs / 12) + " eggs " + eggs % 12);
         }
     }
 }
