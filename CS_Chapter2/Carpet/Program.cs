5b30e3bd576571ebf5b343fa23d790a78c5ff282
CodyAnagnost <codyanagnost@hotmail.com>
2017-02-07 20:14:54 -0500

    yards

--- CS_Chapter2/Carpet/Program.cs
+++ CS_Chapter2/Carpet/Program.cs
@@ -9,9 +9,12 @@ namespace Carpet
     {
         static void Main(string[] args)
         {
-          
-
-
+            int length = 25;
+            int width = 42;
+            int length_yards = length / 3;
+            int width_yards = width / 3;
+            double cost = length_yards * width_yards ;
+            Console.WriteLine("The total cost per square foot is");
         }
     }
 }
