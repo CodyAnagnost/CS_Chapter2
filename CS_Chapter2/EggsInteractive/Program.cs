41e91bf616b7df4c9df24b0484b42852d849f844
CodyAnagnost <codyanagnost@hotmail.com>
2017-02-08 23:10:09 -0500

    eggs interactive

--- CS_Chapter2/EggsInteractive/Program.cs
+++ CS_Chapter2/EggsInteractive/Program.cs
@@ -9,6 +9,22 @@ namespace EggsInteractive
     {
         static void Main(string[] args)
         {
+            
+            int eggs1, eggs2, eggs3, eggs4;
+
+            Console.WriteLine("How many eggs did the first chicken lay?");
+            eggs1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many eggs did the second chicken lay?");
+            eggs2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many eggs did the third chicken lay?");
+            eggs3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many eggs did the fourth chicken lay?");
+            eggs4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(eggs1 + eggs2 + eggs3 + eggs4); 
         }
     }
 }
